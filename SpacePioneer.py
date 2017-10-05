e8a93d645ace8b3da43079f48f6c2fec17c5e068
Peeranat Na nakorn <mfkung_2@hotmail.com>
2017-10-05 21:49:51 +0700

    add asteroid, fire laser beam

--- SpacePioneer.py
+++ SpacePioneer.py
@@ -1,42 +1,71 @@
 import arcade
-from models import Player
+import random
+from models import Player,Falling,Laser
 SCREEN_WIDTH = 480
 SCREEN_HEIGHT = 640
-SPRITE_SCALING = 1
-
+SPRITE_SCALING = 0.7
 
 class SpaceGameWindow(arcade.Window):
     def __init__(self, width, height):
         super().__init__(width, height)
+        self.all_sprites_list = None
+        self.player_sprite = None
+        self.rock_list = None
+        self.laser_list = None
         self.background = None
         self.set_mouse_visible(False)
+        self.score = 0
         arcade.set_background_color(arcade.color.BLACK)
-       
+    
+    def Asteroid(self):
+       for i in range(100):
+
+            rock = Falling("images/Asteroid1.png", SPRITE_SCALING / 2)
+            rock.center_x = random.randrange(SCREEN_WIDTH-10)
+            rock.center_y = random.randrange(SCREEN_HEIGHT, SCREEN_HEIGHT * 50)
+            self.all_sprites_list.append(rock)
+            self.rock_list.append(rock)
+
     def setup(self):
         #Set backgound
         self.background = arcade.load_texture("images/BG.jpg")
         #sprite lists
         self.all_sprites_list = arcade.SpriteList()
-        #Set rocket
-        self.player = Player("images/rocket.png", SPRITE_SCALING)
-        self.player.setup(SCREEN_WIDTH/2, 70, self.all_sprites_list)
+        self.rock_list = arcade.SpriteList()
+        #Set player
+        self.player = Player("images/rocket2.png", SPRITE_SCALING)
+        self.player.setup(SCREEN_WIDTH/2, 60, self.all_sprites_list,self.rock_list, self.score)
         self.all_sprites_list.append(self.player)
-
-    #Key setting
-    def on_key_press(self, key, key_modifiers):
-        self.player.on_key_press(key, key_modifiers)
-    def on_key_release(self, key, modifiers):
-        self.player.on_key_release(key, modifiers)
+        self.laser = Laser("images/laser1.png", SPRITE_SCALING * 1.5)
+        #set asteroid
+        self.Asteroid() 
 
     def on_draw(self):
         arcade.start_render()
         arcade.draw_texture_rectangle(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2,
                                       SCREEN_WIDTH, SCREEN_HEIGHT, self.background)
-        self.player.draw()
+        self.all_sprites_list.draw()
+        output = "Score: {}".format(self.score)
+        arcade.draw_text(output, 10, 20, arcade.color.WHITE, 14)
+    #Key setting
+    def on_key_press(self, key, key_modifiers):
+        self.player.on_key_press(key, key_modifiers)
+
+    def on_key_release(self, key, modifiers):
+        self.player.on_key_release(key, modifiers)
+
+    
+
     def update(self, delta):
-        self.player.update(delta)
+        self.all_sprites_list.update()
         
         
+        '''hit_list = \
+            arcade.check_for_collision_with_list(self.player,
+                                                 self.rock_list)
+        if hit_list:
+            player.kill()'''
+
  
 if __name__ == '__main__':
     window = SpaceGameWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
--- models.py
+++ models.py
@@ -1,21 +1,28 @@
 import arcade.key
+from random import randint
 
-
-
-SCREEN_HEIGHT = 480
-SCREEN_WIDTH = 640
-
-MOVEMENT_SPEED = 4
-
+SCREEN_WIDTH = 480
+SCREEN_HEIGHT = 640
+MOVEMENT_SPEED = 5
+LASER_SPEED = 10
+SPRITE_SCALING = 0.7
 
 #KEY_OFFSET = {arcade.key.UP: DIR_UP,
 #              arcade.key.DOWN: DIR_DOWN,
 #              arcade.key.RIGHT: DIR_RIGHT,
 #              arcade.key.LEFT: DIR_LEFT}
+class Laser(arcade.Sprite):
+    def update(self):
+        self.center_y += LASER_SPEED
+        if self.center_x < -100 or self.center_x > 1500 or \
+                self.center_y > 1100 or self.center_y < -100:
+            self.kill()
 
 class Player(arcade.Sprite):
-    def setup(self, x, y, all_sprites_list):
+    def setup(self, x, y, all_sprites_list, rock_list, score):
         self.all_sprites_list = all_sprites_list
+        self.laser_list = arcade.SpriteList()
+        self.rock_list = rock_list
         self.center_x = x
         self.center_y = y        
 
@@ -24,25 +31,45 @@ class Player(arcade.Sprite):
             self.change_x = MOVEMENT_SPEED
         elif key == arcade.key.LEFT:
             self.change_x = -MOVEMENT_SPEED
+        elif key == arcade.key.SPACE:
+            laser = Laser("images/laser1.png", SPRITE_SCALING * 1.5)
+            laser.center_x = self.center_x
+            laser.bottom = self.top
+
+           
+            self.all_sprites_list.append(laser)
+            self.laser_list.append(laser)    
 
     def on_key_release(self, key, modifiers):
         if key == arcade.key.LEFT or key == arcade.key.RIGHT:
             self.change_x = 0
-
     
-   
-    def update(self, delta):
+    def update(self):
         self.center_x += self.change_x
-        self.center_y += self.change_y
+   
 
-        if self.left < 0:
-            self.left = 0
-        elif self.right > SCREEN_WIDTH - 1:
-            self.right = SCREEN_WIDTH - 1
+        if self.left < -5:
+            self.left = -5
+        if self.right > SCREEN_WIDTH+5:
+            self.right = SCREEN_WIDTH+5
 
-        if self.bottom < 0:
-            self.bottom = 0
-        elif self.top > SCREEN_HEIGHT - 1:
-            self.top = SCREEN_HEIGHT - 1
+        for laser in self.laser_list:
+            hit_list = arcade.check_for_collision_with_list(laser,
+                                                            self.rock_list)
+            if len(hit_list) > 0:
+                laser.kill()
+            for rock in hit_list:
+                rock.kill()
+                #score += 1
+####    Asteroid ####
+class Falling(arcade.Sprite):
+    def update(self):
+        self.center_y -= 3
+        if self.top < 0:
+            self.bottom = SCREEN_HEIGHT
+
+
+
+     
 
       
